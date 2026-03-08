17a1ea5c13af2c986e03942936c631ba70793761
דור טאייב <dortayeb40@gmail.com>
2026-03-08 18:41:54 +0000

    File changes

--- src/components/faults/ActiveAlertsList.jsx
+++ src/components/faults/ActiveAlertsList.jsx
@@ -22,6 +22,8 @@ const TYPE_LABELS = {
   inverter_fault: 'תקלת ממיר',
   string_mismatch: 'חוסר התאמת סטרינג',
   phase_voltage_out_of_range: 'מתח פאזה חריג',
+  communication_fault: 'תקלת תקשורת',
+  mid_day_power_drop: 'נפילת מתח באמצע היום',
   other: 'אחר'
 };
 
